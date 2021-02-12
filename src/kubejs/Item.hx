b73cc5af684e95e23e959a2db995e640c8429467
LordMZTE <lord@mzte.de>
2021-02-12 13:42:27 +0100

    fix item being called incorrectly

--- src/kubejs/Item.hx
+++ src/kubejs/Item.hx
@@ -4,7 +4,7 @@ import haxe.extern.EitherType;
 
 typedef DynItem = EitherType<String, Item>;
 
-extern class Item {
+@:native("Item") extern class Item {
 	static function of(id:String, ?count:Int, ?nbt:Dynamic):Item;
 	function withChance(chance:Float):Item;
 	function toResultJson():Dynamic;
